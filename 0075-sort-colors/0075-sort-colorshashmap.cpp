7876ee4c7a8456bf448d4c3fbc41bd22864dfcae
Shreyash S <96829856+Slay-MIT@users.noreply.github.com>
2024-06-08 09:32:17 +0530

    Time: 3 ms (31.94%), Space: 10.2 MB (5.73%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colorshashmap.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(auto it: nums){
+            mp[it]++;
+        }
+        int zero = mp[0];
+        int one = mp[1];
+        int two = mp[2];
+
+        for(int i=0; i<nums.size(); i++){
+            if(zero != 0){
+                nums[i] = 0;
+                zero--;
+            }
+            else if(one != 0){
+                nums[i] = 1;
+                one--;
+            }
+            else{
+                nums[i] = 2;
+                two--;
+            }
+        }
+        for(auto it: nums){
+            cout<<it;
+        }
+    }
+};
