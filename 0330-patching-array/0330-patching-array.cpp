594aeb57d861107c2c58ebbee3c7245402cfdd3e
Shreyash S <96829856+Slay-MIT@users.noreply.github.com>
2024-06-16 18:07:49 +0530

    Time: 9 ms (21.31%), Space: 13.8 MB (45.41%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.cpp
@@ -0,0 +1,22 @@
+//solutions copied
+
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long missing = 1;
+        int patches = 0;
+        int i = 0;
+
+        while (missing <= n) {
+            if (i < nums.size() && nums[i] <= missing) {
+                missing += nums[i];
+                i++;
+            } else {
+                missing += missing;
+                patches++;
+            }
+        }
+
+        return patches;
+    }
+};
