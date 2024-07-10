c43a8d2c4dc4aded63bf7937e654472197577258
Shreyash S <96829856+Slay-MIT@users.noreply.github.com>
2024-07-10 22:45:55 +0530

    Time: 0 ms (100%), Space: 13.9 MB (84.88%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int count = 0;
+        int outCount = 0;
+        int remainCount = 0;
+        for(int i=0; i<logs.size(); i++){
+            if(logs[i].compare("../")==0 && count==0){
+                continue;
+            }
+            if(logs[i].compare("./") == 0){
+                continue;
+            }
+            else if(logs[i].compare("../") == 0 && count!=0){
+                count--;
+            }
+            else{
+                count++;
+            }
+        }
+        return count;
+
+    }
+};
