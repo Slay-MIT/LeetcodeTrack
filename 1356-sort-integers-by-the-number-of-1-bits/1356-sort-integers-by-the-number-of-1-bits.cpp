3883dc896af31de7e49c7c2defea9f04477dfad3
Shreyash S. <96829856+Slay-MIT@users.noreply.github.com>
2026-02-25 22:36:54 +0530

    Time: 16 ms (5.69%), Space: 14.3 MB (43.16%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    //  static bool compare(int a, int b) {
+    //     if (__builtin_popcount(a) == __builtin_popcount(b)) {
+    //         return a < b;
+    //     }
+        
+    //     return __builtin_popcount(a) < __builtin_popcount(b);
+    // }
+    
+    // vector<int> sortByBits(vector<int>& arr) {
+    //     sort(arr.begin(), arr.end(), compare);
+    //     return arr;
+    // }
+
+    static int findWeight(int num) {
+        int mask = 1;
+        int weight = 0;
+        
+        while (num > 0) {
+            if ((num & mask) > 0) {
+                weight++;
+                num ^= mask;
+            }
+            
+            mask <<= 1;
+        }
+        
+        return weight;
+    }
+    
+    static bool compare(int a, int b) {
+        if (findWeight(a) == findWeight(b)) {
+            return a < b;
+        }
+        
+        return findWeight(a) < findWeight(b);
+    }
+    
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), compare);
+        return arr;
+    }
+
+};
